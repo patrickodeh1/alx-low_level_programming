3f18075cb74bc4a1c7c0c43c98f17e867ea75fce
patrick Odeh <vyltyan@gmail.com>
2023-07-14 07:03:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+	putchar(10);
+	return (0);
+}
