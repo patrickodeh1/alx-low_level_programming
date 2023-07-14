aff41769db9e8d8b0d3b9fb2ee55acf15247dc51
patrick Odeh <vyltyan@gmail.com>
2023-07-13 17:57:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char my_str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my_str);
+
+	return (0);
+}
