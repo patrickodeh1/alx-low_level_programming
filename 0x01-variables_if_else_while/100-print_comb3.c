05be58fd71ed476e991d9cb814b46f6296c39f8d
patrick Odeh <vyltyan@gmail.com>
2023-07-16 07:44:46 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,22 @@
 
 int main(void)
 {
-	int num;
+	int num1, num2;
 
-	for (num = '0'; num < '9'; num++)
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		putchar(num);
-		putchar(num + 1);
-		if (num < '8')
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (num1 != num2)
+			{
+				putchar(num1);
+				putchar(num2);
+			}
+			if (num1 < '8' || num2 < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar(10);
