69c4d144f8ea0dc33be78fbc7d04f988ed112c47
patrick Odeh <vyltyan@gmail.com>
2023-07-19 01:59:47 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,14 @@
 
 int print_last_digit(int num)
 {
-	int a;
-
 	if (num >= 0)
 	{
-		_putchar(num % 10 + '0');
+		_putchar((num % 10) + '0');
 	}
 	else
 	{
-		a = -(num % 10);
-		_putchar(a + '0');
+		num *= -1;
+		_putchar((num % 10) + '0');
 	}
 	return (num  % 10);
 }
