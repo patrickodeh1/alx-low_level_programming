2192eb9feefd6dfe7df860caf3e6f57ec4fd5063
patrick Odeh <vyltyan@gmail.com>
2023-07-25 08:38:48 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * _strlen - prints string in reverse order
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * print_rev - prints string in reverse order
  * @s: string
  */
