37aa61e36055c2a596fb88e44ffc48c0963eeed6
patrick Odeh <vyltyan@gmail.com>
2023-07-29 06:12:58 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "maini.h"
+
+/**
+ *  reverse_array -reverses an array
+ *  @a: array
+ *  @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, srt, end;
+
+	end = n - 1;
+	while (srt < end)
+	{
+		tmp = a[srt];
+		a[srt] = a[end];
+		a[end] = tmp;
+		srt++;
+		end--;
+	}
+}
