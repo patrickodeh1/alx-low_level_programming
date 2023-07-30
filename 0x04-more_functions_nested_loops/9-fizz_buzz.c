9eaf079794f7803ebea1b9a026b14486e8c8b7d7
patrick Odeh <vyltyan@gmail.com>
2023-07-30 07:28:02 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,26 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 
+		if (i > 1 && i < 100)
+		{
+			putchar(32);
+		}
+
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 	}
 	putchar(10);
