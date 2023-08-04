9094fa69980c7845214b4076b4eedad8fcdcf9e1
patrick Odeh <vyltyan@gmail.com>
2023-08-04 04:54:10 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, mul;
+
+	if ((argc < 3) || (argc > 3))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		mul = i * j;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
