b7723980ae16411820d88730ce6b895a9a444960
Patrick Odeh <podeh4300@gmail.com>
2023-08-11 05:04:26 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
@@ -30,13 +30,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     }
     ptr[i] = '\0';
     return (ptr);
-}
-int main(void)
-{
-    char *concat;
-
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
 }
