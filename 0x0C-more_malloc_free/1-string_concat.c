7747864a01365b728a0ee120056dcfc5051ba901
Patrick Odeh <podeh4300@gmail.com>
2023-08-11 04:59:42 +0000

    1-string_concat.c

---
+++ 0x0C-more_malloc_free/1-string_concat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    unsigned int i = 0, j = 0, l1 = 0, l2 = 0;
+    char *ptr;
+    
+    while (s1 && s1[l1])
+        l1++;
+    while (s2 && s2[l2])
+        l2++;
+    if (n >= l2)
+        ptr = malloc(sizeof(char) * l2 + 1);
+    else
+        ptr = malloc(sizeof(char) * (l1 + l2 + 1));
+    if (!ptr)
+        return (NULL);
+    while (i < l1)
+    {
+        ptr[i] = s1[i];
+        i++;
+    }
+    while(n < l2 && i < (l1 + n))
+    {
+        ptr[i++] = s2[j++];
+    }
+    ptr[i] = '\0';
+    return (ptr);
+}
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
+}
