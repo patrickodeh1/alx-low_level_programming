3bc34db0583eeed770be199977b803791f8d88bf
patrick Odeh <vyltyan@gmail.com>
2023-08-14 13:59:18 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+## 0x0E-structures_typedef
+
+Directory for c structures
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: this structure defines a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
