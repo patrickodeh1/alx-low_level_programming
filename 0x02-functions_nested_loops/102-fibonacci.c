020062b88bccbef7b52a3ff6a18ac563389ff3db
patrick Odeh <vyltyan@gmail.com>
2023-08-21 13:30:29 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,22 +7,25 @@
 
 int main(void)
 {
-	unsigned long int f1, f2, fn;
+	long int f1, f2, fn;
 	int i;
 
-	f1 = 0;
-	f2 = 1;
+	f1 = 1;
+	f2 = 2;
 	printf("%lu, %lu, ", f1, f2);
 	fn = f1 + f2;
-	for (i = 3; i <= 52; i++)
+	for (i = 3; i <= 50; i++)
 	{
-		printf("%lu", fn);
+		if (i == 50)
+		{
+			printf("%lu \n", fn);
+		}
+		else
+			printf("%lu, ", fn);
+
 		f1 = f2;
 		f2 = fn;
 		fn = f1 + f2;
-		if (i < 52)
-			printf(", ");
 	}
-	putchar(10);
 	return (0);
 }
