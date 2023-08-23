be937629c7efcd2ad7923518d8a3115453ec90ca
patrick Odeh <vyltyan@gmail.com>
2023-08-23 12:20:31 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *ptr;
 
-	if (!s1 || !s2)
-		return (NULL);
 	i = j = k = 0;
+
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
