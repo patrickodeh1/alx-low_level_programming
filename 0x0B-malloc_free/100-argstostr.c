234226b9535693222c65251efa3fdf380663f511
patrick Odeh <vyltyan@gmail.com>
2023-08-23 20:13:26 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,12 +11,24 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i = 0, j = 0, k = 0;
+	int ch = 0, i = 0, j = 0, k = 0;
 	char *s;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	while (i < ac)
+	{
+		while (av[i][j])
+		{
+			ch++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	s = malloc((sizeof(char) * ch) + ac + 1);
+	i = 0;
+	while (av[i])
 	{
 		while (av[i][j])
 		{
