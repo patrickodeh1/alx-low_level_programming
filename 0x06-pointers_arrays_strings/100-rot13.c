a8d016e09e270f5019f8a9c41f0360d77d36d2c3
patrick Odeh <vyltyan@gmail.com>
2023-08-27 09:32:33 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - rotates every character of a string 13 places
+ * @str: string to rotate
+ * Return: rotated string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char alph1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alph2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alph1[j])
+			{
+				str[i] = alph2[j];
+			}
+		}
+	}
+	return (str);
+}
