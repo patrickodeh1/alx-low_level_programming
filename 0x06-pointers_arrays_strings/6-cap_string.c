0d6e999417530af558772574dfcfdaa5e42f54d5
patrick Odeh <vyltyan@gmail.com>
2023-08-27 06:57:54 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,20 +17,20 @@ char *cap_string(char *str)
 			str[i] = toupper(str[i]);
 		}
 		else if (str[i] == ' ' ||
-		str[i] == '.' ||
-		str[i] == ',' ||
-		str[i] == '\n' ||
-		str[i] == '\t' ||
-		str[i] == '!' ||
-		str[i] == '?' ||
-		str[i] == '"' ||
-		str[i] == '(' ||
-		str[i] == ')' ||
-		str[i] == '{' ||
-		str[i] == '}' ||
-		str[i] == ';')
+			str[i] == '.' ||
+			str[i] == ',' ||
+			str[i] == '\n' ||
+			str[i] == '\t' ||
+			str[i] == '!' ||
+			str[i] == '?' ||
+			str[i] == '"' ||
+			str[i] == '(' ||
+			str[i] == ')' ||
+			str[i] == '{' ||
+			str[i] == '}' ||
+			str[i] == ';')
 		{
-			 str[i + 1] = toupper(str[i + 1]);
+			str[i + 1] = toupper(str[i + 1]);
 		}
 	}
 	return (str);
