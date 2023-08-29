e354c3aed5aac230d85b29d77f7929309c617d74
patrick Odeh <vyltyan@gmail.com>
2023-08-29 10:47:36 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints chess board
+ * @a: array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar(10);
+	}
+}
