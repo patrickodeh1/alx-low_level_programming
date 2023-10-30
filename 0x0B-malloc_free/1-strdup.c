af0d5e3b602016068825695e02cb12579f8f9531
Patrick Odeh <vyltyan@gmail.com>
2023-10-30 16:10:13 +0100

    memory allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a new string which is a duplicate of the string str
+ * _strdup - returns pointer to a new string which is duplicate of str
  * @str: string
  * Return: pointer
  */
 char *_strdup(char *str)
 {
 	char *new_str;
+	size_t len = strlen(str);
 
 	if (str == NULL)
 		return (NULL);
-	new_str = (char *)malloc(sizeof(strlen(str) + 1));
+	new_str = (char *)malloc(sizeof(len + 1));
 	if (new_str == NULL)
 		return (NULL);
 	strcpy(new_str, str);
