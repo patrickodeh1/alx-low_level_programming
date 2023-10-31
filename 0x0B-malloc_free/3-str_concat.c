97c6b49bd5507fc062f9e838e0db9628ee27e780
Patrick <vyltyan@gmail.com>
2023-10-31 16:18:04 +0100

    memory allocation

---
+++ 0x0B-malloc_free/3-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * str_concat - function that concatenates string
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	ptr = (char *)malloc(sizeof(strlen(s1) + strlen(s2) + 1));
+	if (ptr)
+	{
+		strcpy(ptr, s1);
+		strcat(ptr, s2);
+	}
+	return (ptr);
+}
