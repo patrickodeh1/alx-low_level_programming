8ee923fd3d8418e4d967b59009c730ecf77bd1b0
Patrick <vyltyan@gmail.com>
2023-11-02 01:32:07 +0100

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,63 +8,38 @@
 
 char **strtow(char *str)
 {
-    if (str == NULL || *str == '\0') {
-        return (NULL);
-    }
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
 
-    int word_count = 0;
-    int in_word = 0;
-    int word_length = 0;
+	int count = 0, i, index = 0, j;
+	char **array, *token;
 
-    // Count the number of words and their lengths
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ' && !in_word) {
-            in_word = 1;
-            word_count++;
-        }
-        if (str[i] == ' ' && in_word) {
-            in_word = 0;
-        }
-        if (in_word) {
-            word_length++;
-        }
-    }
-
-    char **words = (char **)malloc((word_count + 1) * sizeof(char *));
-    if (words == NULL) {
-        return (NULL);
-    }
-
-    int word_index = 0;
-    in_word = 0;
-    word_length = 0;
-
-    // Allocate memory for each word and copy them
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ' && !in_word) {
-            in_word = 1;
-            word_length = 0;
-            words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-            if (words[word_index] == NULL) {
-                for (int j = 0; j < word_index; j++) {
-                    free(words[j]);
-                }
-                free(words);
-                return (NULL);
-            }
-        }
-        if (str[i] == ' ' && in_word) {
-            in_word = 0;
-            word_length = 0;
-            word_index++;
-        }
-        if (in_word) {
-            words[word_index][word_length] = str[i];
-            word_length++;
-            words[word_index][word_length] = '\0';
-        }
-    }
-
-    words[word_count] = NULL;
-    return (words);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			count++;
+			while (str[i] != ' ' && stri != '\0')
+				i++;
+		}
+	}
+	array = (char **)malloc((count + 1) * sizeof(char *));
+	if (array == NULL)
+		return (NULL);
+	token = strtok(str, " ");
+	while (token != NULL)
+	{
+		array[index] = strdup(token);
+		if (array[index] == NULL)
+		{
+			for (j = 0; j < index; j++)
+				free(array[j]);
+			free(array);
+			return (NULL);
+		}
+		index++;
+		token = strtok(NULL, " ");
+	}
+	array[index] = NULL;
+	return (array);
 }
