ec660153fb398920ab64db333c8becbc8e8647ce
Patrick Odeh <vyltyan@gmail.com>
2023-11-06 20:50:42 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,14 +11,14 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if ((*d).name == "")
+		if ((*d).name == NULL || (*d).name == "")
 			printf("Name : (nil)\n");
 		else
 			printf("Name : %s\n", (*d).name);
 
 		printf("Age : %f\n", (*d).age);
 
-		if ((*d).owner == "")
+		if ((*d).owner == NULL || (*d).owner == "")
 			printf("Owner : (nil)\n");
 		else
 			printf("Owner : %s\n", (*d).owner);
