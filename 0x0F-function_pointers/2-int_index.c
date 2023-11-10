222309a710032d7b6d8ff7429fba82d5147c46cb
Patrick Odeh <vyltyan@gmail.com>
2023-11-10 09:00:52 -0800

    struct

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0)
-		return -1;
+		return (-1);
 	if (array != NULL && cmp != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
