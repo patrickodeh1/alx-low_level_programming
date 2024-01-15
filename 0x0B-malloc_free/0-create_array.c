161206d7f022334969e50c06aea4995a3d60589a
Patrick Odeh <pat@DESKTOP-EQKLEBA>
2024-01-15 12:59:29 +0100

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <main.h>
+
+/**
+ * create_array - Function thats create an array
+ * @size: size of array
+ * @c: character to initialize array
+ * Return: pointer to array if successful or null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	ar = malloc(sizeof(char) * (size + 1));
+	if (ar == NULL)
+		return (NULL);
+	i = 0;
+	while (size > i)
+	{
+		ar[i] = c;
+		i++;
+	}
+	return (ar);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+## 0x0B-malloc_free
+
+### Dynamic memory allocation
+
+- Malloc
+- free
+
