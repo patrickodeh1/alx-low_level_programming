9b6c594c14c7e0037e9a16dcb2c10cb7cfca53f4
Patrick Odeh <vyltyan@gmail.com>
2024-01-18 12:32:24 +0100

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  free_grid - frees the array created by alloc_grid
+ *  @grid: array to free
+ *  @height: height of the array
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++);
+		free(grid);
+}	
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -4,4 +4,3 @@
 
 - Malloc
 - free
-
