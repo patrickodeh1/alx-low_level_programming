f2244f5e4808f8b117aa233efcc35382e5e9c95b
Patrick Odeh <vyltyan@gmail.com>
2024-03-04 13:19:59 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, res;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	else
+	{
+		res = a * b;
+		printf("%d\n", res);
+	}
+	return(0);
+}	
