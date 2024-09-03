0dd0978ac3fcb37f9d07fb57ff4505efc1bba16f
Patrick Odeh <vyltyan@gmail.com>
2024-09-03 19:56:47 +0100

    1-list_len.c

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
+```asm
 section .data
-    fmt db 'Hello, Holberton', 10, 0  ; The format string with a newline character and null terminator
+    msg db 'Hello, Holberton', 10 ; message to print with newline
 
 section .text
     global main
     extern printf
 
 main:
-    ; Set up the argument for printf
-    mov rdi, fmt       ; Address of the format string in rdi (first argument)
-
     ; Call printf
-    call printf
+    lea rdi, [msg]      ; Load address of msg into rdi
+    xor rax, rax       ; Clear rax (no floating-point arguments)
+    call printf         ; Call printf
 
-    ; Exit the program
-    mov eax, 0         ; Return code 0
-    ret                ; Return from main
+    ; Exit program
+    mov eax, 0         ; Return 0
+    ret
+```
