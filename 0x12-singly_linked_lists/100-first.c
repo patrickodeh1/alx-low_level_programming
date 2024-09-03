0377864fed22a703a3e1f2a7fcbd8a7e3cdcba17
Patrick Odeh <vyltyan@gmail.com>
2024-09-03 19:29:38 +0100

    1-list_len.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_before_main - Prints a message before the main function.
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore
+		my house upon my back!\n");
+}
