44a381437bb3c067efcbc47ffc2b9228b7b68f1f
Patrick Odeh <vyltyan@gmail.com>
2024-09-05 11:25:20 +0100

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/* Function prototypes for file I/O */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-/* Function prototype for ELF header */
-void print_elf_header(const unsigned char *e_ident);
+void error_exit(int code, const char *message);
 
 #endif /* MAIN_H */
