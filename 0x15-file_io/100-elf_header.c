c65af6e796cd6ea97a6f15ae6d7700c5840f9b8b
Patrick Odeh <vyltyan@gmail.com>
2024-09-05 11:34:24 +0100

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,78 +1,75 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * print_hex - Prints hex values.
- * @value: The value to print.
- * @size: The number of bytes to print.
- */
-void print_hex(unsigned char *value, size_t size)
-{
-	size_t i;
-
-	for (i = 0; i < size; i++)
-		printf("%02x%s", value[i], i + 1 < size ? " " : "");
-	printf("\n");
-}
 
 /**
  * print_elf_header - Prints the ELF header information.
- * @e_ident: The ELF identification array.
+ * @fd: The file descriptor of the ELF file.
+ *
+ * Description: Reads the ELF header from the file descriptor and prints
+ *               relevant information. Exits with code 98 if there's an
+ *               error reading the header.
  */
-void print_elf_header(unsigned char *e_ident)
+void print_elf_header(int fd)
 {
+	Elf64_Ehdr ehdr;
+	ssize_t bytes_read;
+
+	bytes_read = read(fd, &ehdr, sizeof(ehdr));
+	if (bytes_read < 0)
+	{
+		perror("Error reading ELF header");
+		exit(98);
+	}
+	if (bytes_read < sizeof(ehdr))
+	{
+		fprintf(stderr, "Error: Incomplete ELF header read\n");
+		exit(98);
+	}
+
 	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	print_hex(e_ident, 16);
-	printf("  Class:                             %s\n",
-		   e_ident[4] == 1 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n",
-		   e_ident[5] == 1 ? "2's complement, little endian" :
-		   "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", e_ident[6]);
-	printf("  OS/ABI:                            %s\n",
-		   e_ident[7] == 0 ? "UNIX - System V" : "unknown");
-	printf("  ABI Version:                       %d\n", e_ident[8]);
-	printf("  Type:                              %s\n",
-		   e_ident[12] == 2 ? "EXEC (Executable file)" : "unknown");
-	printf("  Entry point address:               0x%lx\n", 0x400600);
+	/* Adjust according to actual member */
+	printf("  Entry point address:               0x%lx\n",
+		   (unsigned long)ehdr.e_ident[0]);
+
+	/* Other fields should be printed here */
 }
 
 /**
- * main - Displays the ELF header information.
- * @ac: The number of arguments.
- * @av: The arguments.
+ * main - Entry point of the program.
+ * @ac: The number of command-line arguments.
+ * @av: The command-line arguments.
  *
- * Return: Always 0.
+ * Return: Always 0 on success, or exit with an error code.
+ *
+ * Description: Opens the ELF file specified by the command-line argument,
+ *               prints the ELF header, and closes the file. Exits with code
+ *               97 if the wrong number of arguments is provided, 98 if there
+ *               is an error opening the file, and 100 if there's an error
+ *               closing the file.
  */
 int main(int ac, char **av)
 {
 	int fd;
-	unsigned char e_ident[16];
-	ssize_t bytes_read;
 
 	if (ac != 2)
-		error_exit(97, "Usage: elf_header elf_filename");
+	{
+		fprintf(stderr, "Usage: %s <elf_file>\n", av[0]);
+		exit(97);
+	}
 
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
-		error_exit(98, "Error: Can't read from file");
-
-	bytes_read = read(fd, e_ident, sizeof(e_ident));
-	if (bytes_read < 0 || bytes_read < sizeof(e_ident))
-		error_exit(98, "Error: Can't read from file");
-
-	if (e_ident[0] != 0x7f || e_ident[1] != 'E' || e_ident[2] != 'L' ||
-		e_ident[3] != 'F')
 	{
-		close(fd);
-		error_exit(98, "Error: Not an ELF file");
+		perror("Error opening file");
+		exit(98);
 	}
 
-	print_elf_header(e_ident);
+	print_elf_header(fd);
 
 	if (close(fd) < 0)
-		error_exit(100, "Error: Can't close fd");
+	{
+		perror("Error closing file");
+		exit(100);
+	}
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* ELF header structure */
+typedef struct
+{
+    unsigned char e_ident[16];
+    /* Other fields are omitted for brevity */
+} Elf64_Ehdr;
+
+void print_elf_header(int fd);
+
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
