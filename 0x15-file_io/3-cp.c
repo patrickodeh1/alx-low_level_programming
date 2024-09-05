79432bc1241a97272585509cdb7929d35935e6bd
Patrick Odeh <vyltyan@gmail.com>
2024-09-05 11:36:48 +0100

    100-elf_header.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,63 +1,66 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 /**
- * error_exit - Handles errors and exits with a given code.
- * @code: The exit code.
+ * error_exit - Handles errors by printing a message and exiting.
  * @message: The error message to print.
+ * @file: The file name related to the error.
+ * @exit_code: The exit code to use.
  */
-void error_exit(int code, const char *message)
+void error_exit(const char *message, const char *file, int exit_code)
 {
-	if (message)
-		dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
+	dprintf(STDERR_FILENO, "Error: %s %s\n", message, file);
+	exit(exit_code);
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @ac: The number of arguments.
- * @av: The arguments.
+ * main - Copies the contents of one file to another.
+ * @argc: The number of command-line arguments.
+ * @argv: The command-line arguments.
  *
- * Return: Always 0.
+ * Return: 0 on success, or exit with an error code.
  */
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int fd_from, fd_to;
+	int fd_from, fd_to, bytes_read, bytes_written;
 	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
+	struct stat st;
+	mode_t file_perm;
 
-	if (ac != 3)
-		error_exit(97, "Usage: cp file_from file_to");
+	if (argc != 3)
+		error_exit("Usage: ./3-cp file_from file_to", "", 97);
 
-	fd_from = open(av[1], O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from < 0)
-		error_exit(98, "Error: Can't read from file");
+		error_exit("Can't read from file", argv[1], 98);
 
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fstat(fd_from, &st) < 0)
+		error_exit("Can't get file permissions", argv[1], 98);
+
+	file_perm = st.st_mode;
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
 	if (fd_to < 0)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to file_to");
-	}
+		error_exit("Can't write to file", argv[2], 99);
 
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to file_to");
-		}
+			error_exit("Can't write to file", argv[2], 99);
 	}
-
 	if (bytes_read < 0)
-		error_exit(98, "Error: Can't read from file");
+		error_exit("Can't read from file", argv[1], 98);
 
 	if (close(fd_from) < 0)
-		error_exit(100, "Error: Can't close fd_from");
+		error_exit("Can't close fd", argv[1], 100);
 
 	if (close(fd_to) < 0)
-		error_exit(100, "Error: Can't close fd_to");
+		error_exit("Can't close fd", argv[2], 100);
 
 	return (0);
 }
